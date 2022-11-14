7a56a09a19bb69d0e3268ce24faf3bae4c18a6c8
Tracy Pham <phamtracy95@gmail.com>
2022-11-14 14:51:25 -0800

    Updated schema and seeds for employee table, added queries to view depts, roles, employees

--- employee_db/schema.sql
+++ employee_db/schema.sql
@@ -27,6 +27,6 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id)
     REFERENCES role(id),
     FOREIGN KEY (manager_id)
-    REFERENCES role(id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
-);
+)
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const inquirer = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 const { connect } = require("http2");
+const Employee = require("./lib/Employee");
+const Department = require("./lib/Department");
 // const Employee = require("Employee");
 
 const allDepartments = [];
@@ -103,15 +105,11 @@ init();
 function init() {
   inquirer.prompt(menu).then((response) => {
     if (response.menu === "View all departments") {
-      db.query("SELECT * FROM department", function (err, response) {
-        console.table(response);
-      });
-    } else if (response.menu === "View all Roles") {
-      console.log("Display all roles with SQL");
-      // SELECT * from role
+      viewDepts();
+    } else if (response.menu === "View all roles") {
+      viewRoles();
     } else if (response.menu === "View all employees") {
-      console.log("Display all employees with SQL");
-      // SELECT * from employee
+      viewEmployees();
     } else if (response.menu === "Add a department") {
       addDepartment();
     } else if (response.menu === "Add a role") {
@@ -125,14 +123,47 @@ function init() {
   });
 }
 
+// View all departments
+function viewDepts() {
+  db.query("SELECT * FROM department", function (err, response) {
+    console.table(response);
+    init();
+  });
+}
+
+// View all roles
+function viewRoles() {
+  db.query("SELECT * FROM role", function (err, response) {
+    console.table(response);
+    console.log("VIEW ALL Roles");
+    init();
+  });
+}
+
+// View all employees
+function viewEmployees() {
+  db.query("SELECT * FROM employee", function (err, response) {
+    console.table(response);
+    init();
+  });
+}
+
 // Adds new department
 function addDepartment() {
   inquirer.prompt(addDepartmentQs).then((response) => {
-    console.log(response);
-    allDepartments.push(response);
-    console.log(allDepartments);
-    // console.table(allDepartments);
-    init();
+    // const department = new Department(response.deptName);
+    db.query(
+      "INSERT INTO department (department_name) VALUES (?)",
+      response.deptName,
+      function (err, response) {
+        if (err) return err;
+        console.log("Department added");
+        init();
+      }
+    );
+    // allDepartments.push(department);
+
+    // console.table(allDepartments);å
     // INSERT INTO department (department_name)
     // VALUES (response)
   });
@@ -152,7 +183,14 @@ function addRole() {
 // Adds a new employee
 function addEmployee() {
   inquirer.prompt(addEmployeeQs).then((response) => {
-    console.log(response);
+    const employee = new Employee(
+      response.employeeFirstName,
+      response.employeeLastName,
+      response.employeeRole,
+      response.employeeManager
+    );
+    allEmployees.push(employee);
+    console.log(allEmployees);
     init();
     // INSERT INTO employee (first_name, last_name, role_id, manager_id)
     // VALUES (response.employeeFirstName, response.employeeLastName, response.employeeRole, response.employeeManager)
